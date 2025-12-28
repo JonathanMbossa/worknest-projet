2fd70ceb306dac22d571e8fd6eadb482e4e708aa
jmb_s <jonathan.mbossa@eemi.com>
2025-12-28 22:03:57 +0100

    feat: configuration backend (package.json, jest, variables d'environnement)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/tests/'
+  ],
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.js']
+};
+
